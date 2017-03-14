1f46626849e31fe485afa1d8aa3017aa4879ce94
joemaidman <joemaidman@gmail.com>
2017-03-14 10:48:01 +0000

    Basic functionality passing

--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,7 +1,14 @@
 var fizzBuzz = function(number) {
-  if (number % 3 === 0) {
-    return "Fizz";
-  } else {
+  if (number % 15 === 0) {
+    return "FizzBuzz";
+  }
+  else if (number % 5 === 0) {
     return "Buzz";
+  }
+  else if (number % 3 === 0) {
+    return "Fizz";
+  }
+  else {
+    return number;
   }
 };
